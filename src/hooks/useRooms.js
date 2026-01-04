1f7455047d75f03b1e670a2e65cad820fdaf0f18
Trần Thị Thanh Ngân <22130181@st.hcmuaf.edu.vn>
2026-01-04 21:26:42 +0700

    fix lỗi không lấy được phòng khi tạo

--- src/hooks/useRooms.js
+++ src/hooks/useRooms.js
@@ -1,36 +1,47 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import websocketService from '../services/websocketService';
-import { addRoom, addJoinedRoom } from '../redux/slices/chatSlice';
+import { addRoom, addJoinedRoom, setRoomCreateSuccess, setRoomCreateError } from '../redux/slices/chatSlice';
 
 const useRooms = () => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    // Xử lý khi tạo phòng thành công
-    const handleRoomCreation = (data) => {
-      if (data?.status !== 'error') {
-        const roomName = data?.data?.name || data;
-        dispatch(addRoom({ name: roomName }));
-        // Tự động join vào phòng vừa tạo
-        websocketService.send('JOIN_ROOM', { name: roomName });
-      }
-    };
+    useEffect(() => {
+        const handleRoomCreation = (data) => {
+            if (data?.status === 'error') {
+                const err = data?.mes || data?.message || data?.data?.mes || 'Tạo phòng thất bại';
+                dispatch(setRoomCreateError(err));
+                return;
+            }
+            if (data?.status !== 'error') {
 
-    websocketService.on('CREATE_ROOM', handleRoomCreation);
-    
-    // Xử lý khi đã join vào phòng
-    websocketService.on('JOIN_ROOM', (data) => {
-      const name = data?.data?.name || data?.room;
-      if (name) {
-        dispatch(addJoinedRoom(name));
-      }
-    });
+                const maybe = data?.data ?? data;
+                const roomName = maybe?.name || maybe;
+                // Nếu roomName là object (do server shape unexpected), try to extract string
+                const finalName = typeof roomName === 'string' ? roomName : (roomName?.name || roomName?.toString?.() || undefined);
+                if (finalName) {
+                    dispatch(addRoom({ name: finalName }));
+                    // Thông báo thành công cho UI
+                    dispatch(setRoomCreateSuccess(`Tạo phòng "${finalName}" thành công`));
+                    // Tự động join vào phòng vừa tạo
+                    websocketService.send('JOIN_ROOM', { name: finalName });
+                }
+            }
+        };
 
-    return () => {
-      websocketService.off('CREATE_ROOM', handleRoomCreation);
-    };
-  }, [dispatch]);
+        websocketService.on('CREATE_ROOM', handleRoomCreation);
+
+        websocketService.on('JOIN_ROOM', (data) => {
+            const name = data?.data?.name || data?.room;
+            if (name) {
+                dispatch(addJoinedRoom(name));
+            }
+        });
+
+        return () => {
+            websocketService.off('CREATE_ROOM', handleRoomCreation);
+        };
+    }, [dispatch]);
 };
 
 export default useRooms;
